2f0bf5af992d5472082f77b77fb7e266faa49718
stg25 <stefanoghizzi@yahoo.com>
2019-05-13 15:37:47 +0200

    Gio's Ex

--- index.php
+++ index.php
@@ -1,21 +1,162 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <!-- Handlebars -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
+<?php
 
-    <script id="template" type="text/x-handlebars-template">
+  include "databaseInfo.php";
 
-    </script>
-    <!-- JS -->
-    <script src="script.js"></script>
-    <title>Hotel Join</title>
-  </head>
-  <body>
-    
-  </body>
-</html>
+  class Prenotazione {
+
+    private $id;
+    private $stanzaId;
+    private $configurazioneId;
+    private $createdAt;
+
+    function __construct($id, $stanzaId, $configurazioneId, $createdAt) {
+
+      $this->id = $id;
+      $this->stanzaId = $stanzaId;
+      $this->configurazioneId = $configurazioneId;
+      $this->createdAt = $createdAt;
+    }
+
+    function getId() {
+      return $this->id;
+    }
+    function getStanzaId() {
+      return $this->stanzaId;
+    }
+
+    public static function getAllPrenotazioni($conn) {
+
+      $sql = "
+              SELECT *
+              FROM prenotazioni
+              WHERE MONTH(created_at) = 5
+
+      ";
+
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+        $prenotazioni = [];
+        while ($row = $result->fetch_assoc()) {
+          $prenotazioni[] =
+            new Prenotazione( $row["id"],
+                              $row["stanza_id"],
+                              $row["configurazione_id"],
+                              $row["created_at"]);
+        }
+      }
+
+      return $prenotazioni;
+    }
+  }
+
+  class Stanza {
+
+    private $id;
+    private $roomNumber;
+    private $floor;
+    private $beds;
+
+    function __construct($id, $roomNumber, $floor, $beds) {
+
+      $this->id = $id;
+      $this->roomNumber = $roomNumber;
+      $this->floor = $floor;
+      $this->beds = $beds;
+
+    }
+
+    function getRoomNumber() {
+      return $this->roomNumber;
+    }
+    function getFloor() {
+      return $this->floor;
+    }
+
+    public static function getStanzaById($conn, $id) {
+
+      $sql = "
+
+              SELECT *
+              FROM stanze
+              WHERE id = $id
+
+      ";
+
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $stanza = new Stanza( $row["id"],
+                              $row["room_number"],
+                              $row["floor"],
+                              $row["beds"]);
+      }
+
+      return $stanza;
+    }
+  }
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_errno) {
+
+    echo $conn->connect_error;
+    return;
+  }
+
+  $prenotazioni = Prenotazione::getAllPrenotazioni($conn);
+
+  foreach ($prenotazioni as $prenotazione) {
+
+    $stanzaId = $prenotazione->getStanzaId();
+    $stanza = Stanza::getStanzaById($conn, $stanzaId);
+
+    echo "ID prenotazione: " . $prenotazione->getId() . "<br>" .
+         "Numero stanza: " . $stanza->getRoomNumber() . "<br>" .
+         "Piano stanza: " . $stanza->getFloor() . "<br><br>";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- script.js
+++
@@ -1,6 +0,0 @@
-
-function init() {
-  console.log("OK");
-}
-
-$(document).ready(init);
